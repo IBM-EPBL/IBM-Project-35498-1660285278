ef917ad68b0162caafdcd0a9221832796c4f3dc4
Abishek Kumar <anjaan@OffSec.anjaan.com>
2022-09-27 11:12:58 +0530

    file uploaded

---
+++ Assignment-1/Akash/1.py
@@ -0,0 +1,30 @@
+#creation of list
+akash = []
+print(akash)
+
+#inserting element
+akash.insert(0,1)
+print(akash)
+
+#removing element
+akash.remove(1)
+print(akash)
+
+#appending element
+akash.append(1)
+akash.append(0)
+akash.append(5)
+akash.append(4)
+print(akash)
+
+#sorting list
+akash.sort()
+print(akash)
+
+#popping element
+akash.pop()
+print(akash)
+
+#reversing list
+akash.reverse()
+print(akash)
