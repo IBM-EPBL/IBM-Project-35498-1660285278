2b689e2b9407cb7854735dec92472ceec0fb1c4e
Abishek Kumar <anjaan@OffSec.anjaan.com>
2022-10-16 21:17:00 +0530

    file uploaded

---
+++ Assignments/Team Lead/Assignment 2/1/create.sql
@@ -0,0 +1,41 @@
+CREATE TABLE "TXZ44264"."User Details" ( User CHAR(30) NOT NULL ,Roll INT NOT NULL ,Email CHAR(30) NOT NULL ,Password CHAR(30) NOT NULL )
+INSERT
+  INTO  "TXZ44264"."User Details" ("USER","ROLL","EMAIL","PASSWORD")
+  VALUES(
+'abishek',
+1,
+'abishek@mail.com',
+'abishek1'
+);
+INSERT
+  INTO  "TXZ44264"."User Details" ("USER","ROLL","EMAIL","PASSWORD")
+  VALUES(
+'adharsh',
+2,
+'adharsh@mail.com',
+'adharsh2'
+);
+INSERT
+  INTO  "TXZ44264"."User Details" ("USER","ROLL","EMAIL","PASSWORD")
+  VALUES(
+'akash',
+3,
+'akash@mail.com',
+'akash3'
+);
+INSERT
+  INTO  "TXZ44264"."User Details" ("USER","ROLL","EMAIL","PASSWORD")
+  VALUES(
+'gibson',
+4,
+'gibson@mail.com',
+'gibson4'
+);
+INSERT
+  INTO  "TXZ44264"."User Details" ("USER","ROLL","EMAIL","PASSWORD")
+  VALUES(
+'dummy',
+5,
+'d@mail.com',
+'dummy5'
+);
